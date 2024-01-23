b92728e091e22fff897cdffd256954e5f6ecd670
Hunter Harrison <hunterharrisondev@gmail.com>
2024-01-23 04:31:12 -0600

    Added Procfile

---
+++ main.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const sgMail = require('@sendgrid/mail')
+
+const app = express();
+app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/mailer', (req, res) => {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  const msg = {
+    from: 'hunter@codadevelopment.net',
+    to: 'hunter@codadevelopment.net',
+    subject: 'NEW SUBMISSION FROM WEBSITE',
+    html: `${req.body.name} <br/> ${req.body.email} <br/> ${req.body.phone} <br /> ${req.body.message} `
+  }
+  sgMail.send(msg)
+  .then(() => {
+    res.status(200);
+    res.end()
+  })
+  .catch((error) => {
+    res.status(500);
+    res.end();
+  })
+});
+
+let port = process.env.PORT;
+if (!port) {
+  port = 3000;
+}
+
+app.listen(port);
